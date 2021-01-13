8464177a394513f5dfd7b42cb6d89bc61b842465
Samvita Karkal <samvitaskarkal@gmail.com>
2021-01-13 11:58:39 +0530

    added resume download

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :home
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'download_pdf', to: "homes#download_pdf"
 end
